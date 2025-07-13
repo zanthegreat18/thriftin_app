e298844bee4dd2d1a6e39c03cbec48b7b0a7224e
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-07-13 17:44:41 +0700

    menampilkan hapus dan edit fungsi

--- lib/screens/Product/produk_list_page.dart
+++ lib/screens/Product/produk_list_page.dart
@@ -112,85 +112,104 @@ class _ProductListPageState extends State<ProductListPage> {
   }
 
   Widget _buildProductCard(dynamic product) {
-    return GestureDetector(
-      onTap: () async {
-        // Trigger event ke bloc
-        context.read<ProductBloc>().add(ProductSelected(product));
-
-        // Delay kecil biar state ke-update
-        await Future.delayed(const Duration(milliseconds: 100));
-
-        // Navigasi setelah selectedProduct keisi
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (_) => const ProductDetailPage()),
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          color: const Color(0xFFF8F7FA),
-          borderRadius: BorderRadius.circular(16),
-          boxShadow: const [
-            BoxShadow(
-              color: Color(0x11000000),
-              blurRadius: 8,
-              offset: Offset(0, 4),
-            )
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 120,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.grey[300],
-                borderRadius:
-                    const BorderRadius.vertical(top: Radius.circular(16)),
-                image: product['gambar'] != null
-                    ? DecorationImage(
-                        image: NetworkImage(
-                            'http://10.0.2.2:8000/storage/${product['gambar']}'),
-                        fit: BoxFit.cover,
-                      )
-                    : null,
-              ),
-              child: product['gambar'] == null
-                  ? const Center(
-                      child: Icon(Icons.image_not_supported, size: 40))
-                  : null,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+  return Stack(
+    children: [
+      GestureDetector(
+        onTap: () async {
+          context.read<ProductBloc>().add(ProductSelected(product));
+          await Future.delayed(const Duration(milliseconds: 100));
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => const ProductDetailPage()),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color(0xFFF8F7FA),
+            borderRadius: BorderRadius.circular(16),
+            boxShadow: const [
+              BoxShadow(
+                color: Color(0x11000000),
+                blurRadius: 8,
+                offset: Offset(0, 4),
+              )
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
                 children: [
-                  Text(product['nama_produk'] ?? '',
-                      style: const TextStyle(fontWeight: FontWeight.bold)),
-                  const SizedBox(height: 4),
-                  Text("Rp ${product['harga']}",
-                      style: const TextStyle(color: Colors.grey)),
+                  Container(
+                    height: 120,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+                      image: product['gambar'] != null
+                          ? DecorationImage(
+                              image: NetworkImage('http://10.0.2.2:8000/storage/${product['gambar']}'),
+                              fit: BoxFit.cover,
+                            )
+                          : null,
+                    ),
+                    child: product['gambar'] == null
+                        ? const Center(child: Icon(Icons.image_not_supported, size: 40))
+                        : null,
+                  ),
+                  Positioned(
+                    top: 4,
+                    right: 4,
+                    child: PopupMenuButton<String>(
+                      onSelected: (value) {
+                        if (value == 'edit') {
+                          // Kirim product ke selectedProduct
+                          context.read<ProductBloc>().add(ProductSelected(product));
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (_) => const UploadProductPage()),
+                          );
+                        } else if (value == 'delete') {
+                          context.read<ProductBloc>().add(ProductDeleted(product['id']));
+                        }
+                      },
+                      itemBuilder: (context) => [
+                        const PopupMenuItem(value: 'edit', child: Text('Edit')),
+                        const PopupMenuItem(value: 'delete', child: Text('Hapus')),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            ),
-            const Spacer(),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.black,
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(product['nama_produk'] ?? '', style: const TextStyle(fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 4),
+                    Text("Rp ${product['harga']}", style: const TextStyle(color: Colors.grey)),
+                  ],
                 ),
-                padding: const EdgeInsets.all(8),
-                child: const Icon(Icons.shopping_cart,
-                    size: 18, color: Colors.white),
               ),
-            )
-          ],
+              const Spacer(),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Container(
+                  margin: const EdgeInsets.all(8),
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  child: const Icon(Icons.shopping_cart, size: 18, color: Colors.white),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    );
-  }
+    ],
+  );
+}
 }
