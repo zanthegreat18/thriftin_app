c4fcfd0699e7589521f40f04f5558e97cf4c291c
ZanWhoAsk? <sulistiyofauzan27@gmail.com>
2025-07-13 08:22:25 +0700

    penyesuaian main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:thriftin_app/Produk/bloc/product_bloc.dart';
 import 'package:thriftin_app/bloc/auth_bloc.dart';
 import 'package:thriftin_app/screens/login_page.dart';
-import 'package:thriftin_app/services/auth_service.dart';
+import 'package:thriftin_app/screens/product_detail_page.dart';
 import 'package:thriftin_app/screens/admin_dashboard_page.dart';
 import 'package:thriftin_app/screens/user_dashboard_page.dart';
-
+import 'package:thriftin_app/services/auth_service.dart';
+import 'package:thriftin_app/services/product_service.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final AuthService authService = AuthService();
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AuthBloc(authService),
+    final authService = AuthService();
+    final productService = ProductService();
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthBloc(authService)),
+        BlocProvider(create: (_) => ProductBloc(productService)),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
+        title: 'Thriftin App',
+        theme: ThemeData(
+          scaffoldBackgroundColor: const Color(0xFFFBF9FC),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            elevation: 0,
+          ),
+          fontFamily: 'Roboto',
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.black,
+            secondary: Colors.blueAccent,
+          ),
+        ),
         initialRoute: '/',
         routes: {
-          '/': (_) => LoginPage(),
-          '/admin-dashboard': (_) => AdminDashboardPage(),
-          '/user-dashboard': (_) => UserDashboardPage(),
+          '/': (_) =>  LoginPage(),
+          '/admin-dashboard': (_) =>  AdminDashboardPage(),
+          '/user-dashboard': (_) => const UserDashboardPage(),
+          '/product-detail': (_) => const ProductDetailPage(),
         },
       ),
     );
